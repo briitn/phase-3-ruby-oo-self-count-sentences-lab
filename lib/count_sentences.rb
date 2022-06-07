aa0a737b15bec46af448096e442502b18401f356
princegobah <babynamek@gmail.com>
2022-06-07 07:54:12 -0400

    commit tree 2539ecb25c6695e98eb6c8dacb5d3b4d0d5aeec3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?('.')
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+  if self.end_with?('?')
+  true
+  else
+  false
+ end
   end
 
   def exclamation?
+  if self.end_with?('!')
+  true
 
-  end
-
-  def count_sentences
-
+ else
+  false
   end
 end
+
+
+ def count_sentences
+  empty_array=[]
+gress=self.split(/[!?.]/)
+c_gress=gress.each do |item|
+  if item!=""
+    empty_array.push(item)
+  end
+end
+empty_array.length
+end
+end
+
+puts 'This, well, is a sentence. This is too!! And so is this, I think? Woo...'.count_sentences
